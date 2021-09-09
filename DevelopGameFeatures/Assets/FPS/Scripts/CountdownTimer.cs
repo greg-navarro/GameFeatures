d4629e08d26516713f359e722f7dbc2e4f1bc514
greg-navarro <gregorylitmannavarro@gmail.com>
2021-09-09 12:29:53 -0700

    Add countdown timer asset and object

---
+++ DevelopGameFeatures/Assets/FPS/Scripts/CountdownTimer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountdownTimer : MonoBehaviour
+{
+    float currentTime = 0f;
+    float initialTime = 10f; // TODO take as parameter
+
+    [SerializeField] Text countdownText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = initialTime;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;
+        countdownText.text = currentTime.ToString("0");
+
+        if (currentTime <= 0)
+        {
+            currentTime = 0;
+            // TODO set flag to end current time limited stage
+        }
+    }
+}
